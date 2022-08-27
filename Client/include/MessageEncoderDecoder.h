0944a90b4157f0ec5fc7f624aa90c98c25bd6750
Miryam Ashurov <94792174+miryamAshurov@users.noreply.github.com>
2022-08-27 10:58:28 +0300

    Update MessageEncoderDecoder.h

--- Client/include/MessageEncoderDecoder.h
+++ Client/include/MessageEncoderDecoder.h
@@ -1,6 +1,3 @@
-//
-// Created by omer5 on 01/01/2022.
-//
 #include <iostream>
 #include <vector>
 #include <ctime>
@@ -11,10 +8,8 @@
 using std::string;
 using std::vector;
 
-
 class MessageEncoderDecoder {
 	private:
-
 		static vector<string> split_input(string msg);
 		static string time_to_string(int x);
 		static string get_date_and_time();
@@ -22,14 +17,11 @@ class MessageEncoderDecoder {
 		static string decode_ack_message(char bytes [], unsigned int start, unsigned int bytesToRead);
 		static void shortToBytes(short num, char* bytesArr);
 
-
 	public:
 		MessageEncoderDecoder();
 		static int encode(string msg, char bytes[]);
 		static string decode (char bytes[], unsigned int bytesToRead);
 		static string to_lower(string str);
-
 };
-
 
 #endif //SPL3_CLIENT_MESSAGEENCODERDECODER_H
